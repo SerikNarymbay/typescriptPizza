a31a56c71ad7aa9110e9de496773f5db1c531cf4
Serik <s.narymbay@gmail.com>
2023-12-13 18:48:23 +0600

    add pizzas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import './App.css';
+import React, { FC, useState } from "react";
+import "./App.css";
+import AddPizzaForm from "./components/AddPizzaForm";
+import Pizza from "./models/Pizza";
 
+const App: FC = () => {
+  const [pizzasList, setPizzasList] = useState<Pizza[]>([]);
+  const addPizza = (newPizza: Pizza) => {
+    setPizzasList([...pizzasList, newPizza]);
+  };
 
-function App() {
+  console.log("pizzasList>>>>",pizzasList);
+  
   return (
     <div className="App">
-      New App!
+      <div className="wrap">
+        <span className="heading">Наша пицерия</span>
+        <AddPizzaForm addPizza={addPizza} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
